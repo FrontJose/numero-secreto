1dc050ce8da327dc047334eeac93bf2eee090366
José Marcelo Leandro <106685731+JoseMarceloLeandro@users.noreply.github.com>
2024-03-25 11:27:01 -0300

    finalizado exercicio.

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let totalGeral;
+limpar()
+
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let precoProduto = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let carrinho = document.getElementById('lista-produtos');
+    let preco = precoProduto * quantidade;
+    
+    if (produto != ' ' && quantidade > 0){
+        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
+        </section>`;
+        totalGeral = totalGeral + preco;
+        let campoTotal = document.getElementById('valor-total');
+        campoTotal.textContent = `R$ ${totalGeral}`;
+        document.getElementById('quantidade').value=0;
+    } else{alert('Você deve selecionar um produto e uma quantidade para adicionar ao carrinho!')}
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+}
